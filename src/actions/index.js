3c759d278ee7d2d9d46e3edbcad6dea2e5891644
allison <allisonchen1130@gmail.com>
2020-02-21 23:18:13 -0500

    Fething user data, and display user name

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,15 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async dispatch => {
-  const response = await jsonPlaceholder.get("./posts");
+  const response = await jsonPlaceholder.get("/posts");
 
   dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  // 链接错了 404，4-- client error
+  // server错了
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
 };
